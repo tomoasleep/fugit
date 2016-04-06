c801c96112baf22021f90e24bcf9f8436c79d605
Tomoya Chiba <tomo.asleep@gmail.com>
2016-04-06 21:06:07 +0900

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+var mainWindow = null;
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+  const pagepath = path.normalize(__dirname + '/../static/index.html')
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('file://' + pagepath);
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
